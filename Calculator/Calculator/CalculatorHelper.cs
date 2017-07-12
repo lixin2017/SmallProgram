e01b5ac99c973ab318d8e1d57169ef34fb9c701a
lixin2017 <1289885769@qq.com>
2017-07-12 23:05:09 +0800

    Calculator
    计算器小程序

---
+++ Calculator/Calculator/CalculatorHelper.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Calculator
+{
+    public class CalculatorHelper
+    {
+        double num1, num2, result;
+        string str1;
+        bool judge = true;  //数字输入辅助判断
+        bool judge2 = false; //运算辅助判断
+        
+        //加减乘除方法
+        public void FucClick(TextBox textBox1,string name)
+        {                          
+            try
+            {
+                if (judge2 == true)
+                {
+                    switch (str1)
+                    {
+                        case "Add": num1 += Double.Parse(textBox1.Text);
+                            break;
+                        case "Sub": if (num1 != 0) num1 -= Double.Parse(textBox1.Text);
+                            else num1 = Double.Parse(textBox1.Text);
+                            break;
+                        case "Multiplication": if (num1 != 0) num1 = num1 * Double.Parse(textBox1.Text);
+                            break;
+                        case "Division": if (num1 != 0) num1 = num1 / Double.Parse(textBox1.Text);
+                            break;
+                        case "remainder": if (num1 != 0) num1 = num1 % Double.Parse(textBox1.Text);
+                            break;
+                        default: num1 = Double.Parse(textBox1.Text);
+                            break;
+                    }
+                    textBox1.Text = num1.ToString();
+                    judge = true;
+                    judge2 = false;                    
+                }
+                str1 = name;
+            }
+            catch (Exception)
+            {
+                textBox1.Text = null;
+            }
+        }
+
+        //数字键方法
+        public void Num(TextBox textBox1,string btntext )
+        {            
+            double myNumber = Double.Parse(btntext);
+            if (judge == true)
+            {                                 
+                textBox1.Text = null;
+                judge = false;
+            }           
+            textBox1.Text += myNumber.ToString();                       
+            judge2 = true;          
+        }
+
+        //等于键方法
+        public void Equal(TextBox textBox1)
+        {
+            num2 = Double.Parse(textBox1.Text);
+            if (judge2 == true)
+            try
+            {               
+                switch (str1)
+                {
+                    case "Add": result = num1 + num2;
+                        break;
+                    case "Sub": result = num1 - num2;
+                        break;
+                    case "Multiplication": result = num1 * num2;
+                        break;
+                    case "Division": if (num2 != 0) result = num1 / num2;
+                        else MessageBox.Show("除数不能为0");
+                        break;                   
+                    case null: result = num2;
+                        break;
+                    case "Square": result = num1 * num1;   //Square
+                        break;
+                    case "Reciprocal": result = 1 / num1;
+                        break;
+                    case "remainder": result = num1 % num2;
+                        break;
+                }
+                textBox1.Text = result.ToString();
+                judge = true;
+                judge2 = true;
+                num1 = num2 = 0;
+                str1 = null;
+            }
+            catch (Exception)
+            {
+                textBox1.Text = null;
+            }          
+        }
+
+        //小数点方法
+        public void Point(TextBox textBox1)
+        {
+            string str = ".";
+            if (textBox1.Text.IndexOf(str) == -1 && textBox1.Text !="" || textBox1.Text == "0")
+            {
+                textBox1.Text += str;
+                judge = false;
+            }
+            else if (textBox1.Text.Contains('.'))
+            {
+                MessageBox.Show("数字没有两个小数点!");
+            }
+            
+        }
+
+        //清除键方法
+        public void Clear(TextBox textBox1)
+        {            
+            num1 = 0;
+            num2 = 0;
+            result = 0;
+            textBox1.Text = 0.ToString();
+            judge = true;
+            judge2 = true;
+            str1 = null;
+        }
+
+        //单目运算
+        public void MonocularOperation(TextBox textBox1,string name)
+        {
+            try
+            {
+                switch (str1)
+                {
+                    case "Add": num1 += Double.Parse(textBox1.Text);
+                        break;
+                    case "Sub": if (num1 != 0) num1 -= Double.Parse(textBox1.Text);
+                        else num1 = Double.Parse(textBox1.Text);
+                        break;
+                    case "Multiplication": if (num1 != 0) num1 = num1 * Double.Parse(textBox1.Text);
+                        break;
+                    case "Division": if (num1 != 0) num1 = num1 / Double.Parse(textBox1.Text);
+                        break;
+                    case "remainder": if (num1 != 0) num1 = num1 % Double.Parse(textBox1.Text);
+                        break;
+                    default: num1 = Double.Parse(textBox1.Text);
+                        break;
+                }
+            }
+            catch(Exception)
+            {
+                textBox1.Text = null;
+            }  
+            str1 = name;
+            switch (str1)
+            {
+                case "Square": textBox1.Text = (num1 * num1).ToString();
+                    break;
+                case "reciprocal": if (num1 != 0) textBox1.Text = (1 / num1).ToString();
+                   break;
+            }
+            judge = true;
+        }
+       
+    }
+}
---
+++ Calculator/Calculator/CreateCalculator.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Calculator
+{
+    public class CreateCalculator
+    {
+        [STAThread]
+        public static void Main()
+        {           
+            Application.Run(new Form1());
+            
+        }
+        public class Form1 : Form
+        {                                 
+            public Form1()
+            {            
+                CreateForm();                
+                CreateNumButton();
+                CreateFucButton("+", new int[] { 250, 60 }, "Add",1);
+                CreateFucButton("-", new int[] { 250, 130 }, "Sub",1);
+                CreateFucButton("*", new int[] { 250, 200 }, "Multiplication", 1);
+                CreateFucButton("/", new int[] { 250, 270 }, "Division", 1);
+                CreateFucButton("=", new int[] { 170, 270 }, "Equal", 2);
+                CreateFucButton("C", new int[] { 10, 340 }, "Clear", 3);
+                CreateFucButton(".", new int[] { 90, 270 }, "Point", 4);
+                CreateFucButton("平方", new int[] { 90, 340 }, "Square", 5);
+                CreateFucButton("1/x", new int[] { 250, 340 }, "reciprocal", 5);
+                CreateFucButton("%", new int[] { 170, 340 }, "remainder", 1);
+            }
+
+            
+            
+            //创建功能按钮
+            void CreateFucButton(string op,int[]location,string name,int typeofbutton) 
+            {
+                Button btn = new Button();
+                btn.Text = op;
+                btn.Location = new System.Drawing.Point(location[0], location[1]);
+                btn.Size = new System.Drawing.Size(62,53);
+                btn.Name = name;
+                btn.Tag = typeofbutton;
+                btn.Visible = true;
+                this.Controls.Add(btn);
+                btn.Click += button_Click;
+            }
+           
+            
+            //创建数字按钮
+            void CreateNumButton()
+            {
+                int count = 1;
+                for (int i = 0; i < 3; i++)
+                {                   
+                    for (int j = 0; j < 3; j++)
+                    { 
+                        Button btn = new Button();
+                        btn.Text = count.ToString();
+                        btn.Location = new System.Drawing.Point(10+80*j,60+70*i);
+                        btn.Size = new System.Drawing.Size(62, 53);
+                        btn.Name = count.ToString();
+                        btn.Visible = true;
+                        btn.Tag = 0;
+                        this.Controls.Add(btn);
+                        btn.Click += button_Click;
+                        count++;
+                    } 
+                }
+                Button btn0 = new Button();
+                btn0.Text = 0.ToString();
+                btn0.Location = new System.Drawing.Point(10 , 270);
+                btn0.Size = new System.Drawing.Size(62, 53);
+                btn0.Name = 0.ToString();
+                btn0.Visible = true;
+                btn0.Tag = 0;
+                this.Controls.Add(btn0);
+                btn0.Click += button_Click;
+            }
+
+           
+            //创建窗体
+            public System.Windows.Forms.TextBox textBox1; 
+            void CreateForm()
+            {
+                this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                this.ClientSize = new System.Drawing.Size(340, 421);
+                this.HelpButton = true;
+                this.Name = "Form1";
+                this.Text = "calculator";
+                this.ResumeLayout(false);
+                this.PerformLayout();
+
+                //textBox1
+                this.textBox1 = new System.Windows.Forms.TextBox();
+                this.textBox1.Location = new System.Drawing.Point(12, 12);
+                this.textBox1.Name = "textBox1";
+                this.textBox1.Size = new System.Drawing.Size(304, 21);
+                this.textBox1.TabIndex = 2;
+                this.Controls.Add(textBox1);                
+                this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                this.ClientSize = new System.Drawing.Size(340, 421);
+            }            
+            //按钮事件
+            CalculatorHelper helper = new CalculatorHelper();
+            void button_Click(object sender, EventArgs e)
+            {
+                Button btn = (Button)sender;
+                string buttontype=btn.Tag.ToString();
+                switch (buttontype)
+                {
+                    case "0": helper.Num(textBox1, btn.Text);
+                        break;
+                    case "1": helper.FucClick(textBox1, btn.Name);
+                        break;
+                    case "2": helper.Equal(textBox1);
+                        break;
+                    case "3": helper.Clear(textBox1);
+                        break;
+                    case "4": helper.Point(textBox1);
+                        break;
+                    case "5": helper.MonocularOperation(textBox1, btn.Name);
+                        break;
+                }
+            }
+        }
+
+    }
+}
